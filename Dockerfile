435f4b94d9f2ada82b2af30b5a7a44c17d2d0ae2
Paul Mohr <p.mohr@sopamo.de>
2024-10-22 22:19:45 +0300

    asp

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,28 @@
 # Base image -> https://github.com/runpod/containers/blob/main/official-templates/base/Dockerfile
 # DockerHub -> https://hub.docker.com/r/runpod/base/tags
-FROM runpod/base:0.4.0-cuda11.8.0
+FROM downloads.unstructured.io/unstructured-io/unstructured-api:latest
 
-# The base image comes with many system dependencies pre-installed to help you get started quickly.
-# Please refer to the base image's Dockerfile for more information before adding additional dependencies.
-# IMPORTANT: The base image overrides the default huggingface cache location.
+# Switch to the root user to install system dependencies
+USER root
 
+# Install System Dependencies
+RUN apk add --no-cache supervisor && \
+    pip install setuptools
 
-# --- Optional: System dependencies ---
-# COPY builder/setup.sh /setup.sh
-# RUN /bin/bash /setup.sh && \
-#     rm /setup.sh
-
+# Switch back to the non-root user
+USER ${NB_USER}
 
 # Python dependencies
-COPY builder/requirements.txt /requirements.txt
+COPY requirements.txt /requirements.txt
 RUN python3.11 -m pip install --upgrade pip && \
     python3.11 -m pip install --upgrade -r /requirements.txt --no-cache-dir && \
     rm /requirements.txt
 
-# NOTE: The base image comes with multiple Python versions pre-installed.
-#       It is reccommended to specify the version of Python when running your code.
-
-
 # Add src files (Worker Template)
 ADD src .
 
-CMD python3.11 -u /handler.py
+# Set up supervisord configuration
+COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+
+ENTRYPOINT ["python3.11", "-u", "./handler.py"]
+#CMD python3.11 -u /handler.py
--- src/handler.py
+++ src/handler.py
@@ -1,18 +1,76 @@
-""" Example handler file. """
-
 import runpod
+import requests
+import mimetypes
+import subprocess
+import threading
 
-# If your handler runs inference on a model, load the model here.
-# You will want models to be loaded into memory before starting serverless.
+def stream_reader(stream):
+    while True:
+        line = stream.readline()
+        if not line:
+            break
+        print(line.decode('utf-8'), end='')
 
+# Start the process
+process = subprocess.Popen(
+    ["scripts/app-start.sh"],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE
+)
+
+# Start threads to read from stdout and stderr
+stdout_thread = threading.Thread(target=stream_reader, args=(process.stdout,))
+stderr_thread = threading.Thread(target=stream_reader, args=(process.stderr,))
+
+stdout_thread.start()
+stderr_thread.start()
+
+import requests
+import time
+
+# URL to ping
+url = "http://localhost:8000"
+
+def check_server():
+    try:
+        response = requests.get(url)
+        # Check for a successful response (status codes 200-299)
+        print("Could connect to unstructured api server")
+        return True
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+    return False
+
+# Call the function every 500 ms until it succeeds
+while not check_server():
+    time.sleep(0.5)  # 500 ms
+
+def make_request(params: dict):
+    url = 'http://localhost:8000/general/v0/general'
+    headers = {'accept': 'application/json'}
+
+    # Extract the file path and remove it from params
+    file_path = params.pop('files')
+
+    # Open the file and send the request
+    file_content = requests.get(file_path).content
+
+    file_type, _ = mimetypes.guess_type(file_path)
+    files = {'files': ('filename', file_content, file_type or 'application/octet-stream')}
+    data = {key: str(value) for key, value in params.items()}
+
+    response = requests.post(url, headers=headers, files=files, data=data)
+
+    return response.json()
 
 def handler(job):
+    print("got job")
     """ Handler function that will be used to process jobs. """
     job_input = job['input']
 
-    name = job_input.get('name', 'World')
+    response = make_request(job_input)
 
-    return f"Hello, {name}!"
+    return response
 
-
+print("start server---->")
 runpod.serverless.start({"handler": handler})
